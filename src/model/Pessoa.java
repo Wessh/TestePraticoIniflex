29f4527e6c6d399c88bf1cc2622b5d274e4c0da9
Wesley Ribeiro <wesshbr@gmail.com>
2025-08-29 14:59:39 -0300

    feat(model): criado a classe Pessoa

---
+++ src/model/Pessoa.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public LocalDate getDataNascimento(){
+        return dataNascimento;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Data de Nascimento: " + dataNascimento;
+    }
+}
